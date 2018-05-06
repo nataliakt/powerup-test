93af6fd69d973b1bb1bd24d32c37b6230eeed677
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 17:24:32 -0300

    Update estimate.js

--- estimate.js
+++ estimate.js
@@ -1,7 +1,8 @@
+/* global TrelloPowerUp */
+
 var t = TrelloPowerUp.iframe();
 
 window.estimate.addEventListener('submit', function(event){
-  // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t.set('card', 'shared', 'estimate', window.estimateSize.value)
   .then(function(){
@@ -10,5 +11,11 @@ window.estimate.addEventListener('submit', function(event){
 });
 
 t.render(function(){
-  t.sizeTo('#estimate').done();
+  return t.get('card', 'shared', 'estimate')
+  .then(function(estimate){
+    window.estimateSize.value = estimate;
+  })
+  .then(function(){
+    t.sizeTo('#estimate').done();
+  });
 });
