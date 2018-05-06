88986f2eaf2051f77511fa401f6af55b3b5ac2f2
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 16:56:11 -0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,8 @@
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options){
+    return [{
+      icon: 'https://avatars3.githubusercontent.com/u/9091491?s=40&v=4',
+      text: 'Estimate Size',
+    }];
+  },
+});
