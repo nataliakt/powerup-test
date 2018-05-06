02dcbdab85deee369ba40a37741b8f57b8311992
nataliakt <9091491+nataliakt@users.noreply.github.com>
2018-05-06 17:06:51 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,14 @@
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
-      icon: 'https://avatars3.githubusercontent.com/u/9091491?s=40&v=4',
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
       text: 'Estimate Size',
+      callback: function(t){
+        return t.popup({
+          title: "Estimation",
+          url: 'estimate.html'
+        });
+      }
     }];
-  },
+  }
 });
